4e2c34c192c10317f4679465cd8e75ef18359058
Osmar Menezes da Silva <adm.osmarsilva@gmail.com>
2022-11-18 13:04:35 -0300

    ok

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+console.log('Chegamos!')
+
+//seleção de elementos
+const multiplicationForm = document.querySelector('#multiplication-form')
+const numberInput = document.querySelector('#number')
+const multiplicationInput = document.querySelector('#multiplicator')
+const multiplicationTable = document.querySelector('#multiplication-operations')
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+//funções
+const createTable = (number, multiplicatorNumber) => {
+    console.log(number, multiplicatorNumber);
+    multiplicationTable.innerHTML = "";
+    for(i=1; i<= multiplicatorNumber; i++){
+        const result = number * i;
+        console.log(result);
+        const template = `
+            <div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}</div>
+            </div>
+        `;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+    }
+    console.log(multiplicationTitle)
+    multiplicationTitle.innerText = number;
+}
+
+
+//eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+    createTable(multiplicationNumber, multiplicatorNumber)
+
+});
